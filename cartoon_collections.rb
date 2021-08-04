85b4f25ca4ea556b14eb8e421b67372ebe77fde0
Ivan Candido <ivan.candido@powerhrg.com>
2021-08-04 16:28:19 -0400

    Completed Assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.map.with_index do |dwarve, index|
+    puts "#{index + 1}. #{dwarve}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map do |call|
+    "#{call.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  truthy = calls.map do |call|
+    if call.size > 4
+      true
+    else
+      false
+    end
+  end
+  if truthy.include?(true)
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  found_cheese = cheese.find do |c|
+    cheese_types.include?(c)
+  end
+  found_cheese ? "#{found_cheese}" : nil
 end
